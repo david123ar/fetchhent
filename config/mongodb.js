2cc4dda5f022eb3d6c1fb5e1e79da89b07cb00f0
rishabh-star <risingmerta@gmail.com>
2025-07-02 10:45:36 +0530

    first commit

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,8 @@
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
+require('dotenv-flow').config(); // Better than dotenv
 
+console.log("Mongo URI:", process.env.MONGO_URI); // Confirm it's there
+
+const { MongoClient } = require("mongodb");
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -9,7 +11,7 @@ let db;
 async function connectToDb() {
   if (!db) {
     await client.connect();
-    db = client.db('mydatabase'); // Replace with your database name
+    db = client.db('mydatabase');
   }
   return db;
 }
